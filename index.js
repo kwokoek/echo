df6f542409e82f2b4c1bcf4d5eecf694d87a5bf0
kwokoek <kwokoek@glgroup.com>
2013-09-05 18:00:34 -0500

    snakes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('*', function(request, response) {
-  response.end("Checker snakes - "+request.path.slice(1));
+  response.end("Too many snakes - "+request.path.slice(1));
 });
 
 app.listen(3000);
